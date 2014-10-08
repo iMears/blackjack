348f709f2b9a9f7e01d03b14ff95b6fc67644bac
Maxwell Mears <maxwellmears@gmail.com>
2014-10-07 21:39:43 -0700

    Added a card class

--- blackjack.rb
+++ blackjack.rb
@@ -1,23 +1,85 @@
-#Make a object "card"
-#    properties of card: 
-#     => methods..ace?, face?
-#     => data...suit 0-3, value 0-12
-#     => 
-#make a object "deck"
-#    properties of deck:
-#     => array of cards
-#     => shuffle function
-#     => when we shuffle we will initialize
-#make a set of obejects called "hand"
-#deal cards or not to hand
-#
+class Card
+	# attr_reader :rank, :suit
 
+	SUITS = %w(♠ ♥ ♦ ♣)
+	RANKS = %w(2 3 4 5 6 7 8 9 J Q K A)
 
+	def initialize(rank, suit)
+		@rank = rank
+		@suit = suit
+	end
 
-def card(value,suit)
-	value = 1
-	suit = 0
-	
+	def values
+		if @rank == "K" 
+			return [10]
+		elsif @rank == "Q"
+			return [10]
+		elsif @rank == "J"
+			return [10]
+		elsif @rank == "A"
+			return [1, 11]
+		else 
+			return [@rank.to_i]
+		end
+	end
+
+	def to_s
+		@rank + @suit
+	end
 end
 
+def getYesNo(question)
+	answer = 0
+	until answer == "y" || answer == "n"
+		puts question.to_s + " y/n"
+		answer = gets.chop.downcase.chars.first
+	end
+	return answer
+end
+
+def GetHandTotal(player)
+	sum = 0
+	player.each do |x|
+		sum += x.values[0]
+	end
+	return sum
+end
+
+deck = []
+Card::SUITS.each do |x| 
+	Card::RANKS.each do |y|
+		deck << Card.new(y,x)
+	end 
+end
+
+deck.shuffle!
+
+player = []
+player << deck.pop
+player << deck.pop 
+
+puts ("your hand is: #{player.join(", ")}")
+handTotal = GetHandTotal(player)
+puts ("your total is: #{handTotal}")
+
+hit = getYesNo("would you like to hit?")
+while hit == "y"
+	player << deck.pop
+	handTotal = GetHandTotal(player)
+	if handTotal >= 22
+		puts ("sorry you busted")
+		break
+	end
+	puts("your hand is: #{player.join(", ")}")
+	hit = getYesNo("would you like to hit?")
+end
+
+
+dealer = []
+dealer << deck.pop
+dealer << deck.pop
+
+
+puts ("your hand was: #{player.join(", ")}")
+puts ("dealers hand was: #{dealer.join(", ")}")
 
